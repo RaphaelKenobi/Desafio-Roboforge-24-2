3b4b9ee20c8e9594e7cdc8a0246fb5f84655609d
RaphaelPiloneto <raphael.piloneto@mupisystems.com.br>
2025-04-02 18:00:04 -0300

    :recicyle: atualizacao

--- desafio_movimentacao.py
+++ desafio_movimentacao.py
@@ -13,31 +13,23 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 
-# Inicializa o EV3 e os motores
 ev3 = EV3Brick()
 motor_esquerdo = Motor(Port.B)
 motor_direito = Motor(Port.C)
-
+""""
+variaveis de duracao,velocidade etc, seriam passadas como argumentos para as funcoes de acordo com a necessidade
+"""
 def mover_para_frente(velocidade, duracao):
-    """
-    Move o robô para frente com a velocidade e duração especificadas.
-    """
     motor_esquerdo.run(velocidade)
     motor_direito.run(velocidade)
     wait(duracao)
 
 def virar_90_graus():
-    """
-    Faz o robô virar 90 graus à direita.
-    """
     motor_direito.stop()
     motor_esquerdo.run_time(speed=400, time=2000)
 
 def desenhar_quadrado():
-    """
-    Faz o robô desenhar um quadrado.
-    """
-    for _ in range(4):
+    for i in range(4):
         mover_para_frente(velocidade=200, duracao=3000)
         virar_90_graus()
 
